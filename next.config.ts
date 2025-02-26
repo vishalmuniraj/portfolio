483bf9209cf952589426361b924f219c42fcfee1
vishalmuniraj <mariyaselvam3@gmail.com>
2025-02-26 14:18:05 -0600

    Fix gh-pages issue

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',  // Enables static export
-  basePath: '/portfolio', // Set GitHub repo name as base path
-  assetPrefix: '/portfolio/',
-};
-
-module.exports = nextConfig;
