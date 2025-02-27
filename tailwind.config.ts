0fc647346738dcf373c74b7ed6d7e88f8d5dddcc
vishalmuniraj <mariyaselvam3@gmail.com>
2025-02-27 15:09:49 -0600

    Fixed Tailwind import issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,8 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",  // Ensures all components are included
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",  // Ensure App Router components are included
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}", // If using Pages Router
-    "./components/**/*.{js,ts,jsx,tsx,mdx}", // If you have a components folder
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
